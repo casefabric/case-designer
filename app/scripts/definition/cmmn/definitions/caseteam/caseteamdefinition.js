757181c5ed3030e85592406bb8aa81993d4db384
Thijs Petter <thijspetter@gmail.com>
2021-07-06 14:17:55 +0200

    Always clear name and id from caseteam definition
    This in order to remove the name and id attribute from caseRoles elements created with CMMN1.0 versions of the IDE.
    Fixes #55

--- app/scripts/definition/cmmn/definitions/caseteam/caseteamdefinition.js
+++ app/scripts/definition/cmmn/definitions/caseteam/caseteamdefinition.js
@@ -5,16 +5,17 @@ class CaseTeamDefinition extends UnnamedCMMNElementDefinition {
         this.roles = this.parseElements('role', CaseRoleDefinition);
         if (this.roles.length == 0 && this.name) {
             // This means we bumped into a CMMN 1.0 role
-            
+
             // Since this is single role, the (optional) description is already converted to documentation, but remove that
             //  and let the role conversion create it instead
             if (this.documentation.text) this.documentation.text = '';
             this.importNode.appendChild(CaseTeamDefinition.convertRoleDefinition(importNode));
             this.roles = this.parseElements('role', CaseRoleDefinition);
-            // Clear our name and id element, so that caseteam definition is not accidentally found as a case role element
-            this.name = undefined;
-            this.id = undefined;
-            this.caseDefinition.migrated('Converting CMMN1.0 role');        }
+            this.caseDefinition.migrated('Converting CMMN1.0 role');
+        }
+        // Clear our name and id element, so that caseteam definition is not accidentally found as a case role element
+        this.name = undefined;
+        this.id = undefined;
     }
 
     /**
@@ -31,14 +32,16 @@ class CaseTeamDefinition extends UnnamedCMMNElementDefinition {
         const id = clearAttribute('id');
         const name = clearAttribute('name');
         const description = clearAttribute('description');
-        const optionalDescription = name !== description ? `description="${description}"` : ''; 
+        const optionalDescription = name !== description ? `description="${description}"` : '';
         return XML.parseXML(`<role id="${id}" name="${name}" ${optionalDescription}/>`).documentElement;
     }
 
     createExportNode(parentNode) {
         // Only export if there are actual roles defined in this case
+        console.log("Exporting team; roles length: " + this.roles.length)
         if (this.roles.length) {
             super.createExportNode(parentNode, 'caseRoles', 'roles');
         }
+        console.log("This.exportNode: " + XML.prettyPrint(this.exportNode))
     }
 }
