cbf5b6fd2534404489e4b9804cd0c66c4569896f
jsrosman <jsrosman@gmail.com>
2024-02-14 11:30:54 +0100

    Small UI improvements
    Like default button etc.

--- app/scripts/repository/createnewmodeldialog.js
+++ app/scripts/repository/createnewmodeldialog.js
@@ -2,9 +2,11 @@ class CreateNewModelDialog extends Dialog {
     /**
      * @param {IDE} ide
      * @param {string} label
+     * @param {string} defaultName
      */
-    constructor(ide, label) {
+    constructor(ide, label, defaultName = '') {
         super(ide, label);
+        this.defaultName = defaultName;
     }
 
     get name() {
@@ -17,15 +19,18 @@ class CreateNewModelDialog extends Dialog {
 
     renderDialog() {
         const htmlDialog = $(`
-            <label style="width:150px">Name</label><input class = "inputName"/>
-            <br>
-            <label style="width:150px">Description</label><input class = "inputDescription"/>
-            <br>
-            <br>
-            <button class='buttonOk'>OK</button>
-            <button class='buttonCancel'>Cancel</button>
+            <form>
+                <label style="width:150px">Name</label><input class = "inputName" value="${this.defaultName}">
+                <br>
+                <label style="width:150px">Description</label><input class = "inputDescription"/>
+                <br>
+                <br>
+                <input style="background-color:steelblue; color:#fff" type="submit" class='buttonOk' value="OK"/>
+                <button class='buttonCancel'>Cancel</button>
+            </form>
         `);
         this.dialogHTML.append(htmlDialog);
+        this.dialogHTML.find('input').on('focus', e => e.target.select());
         this.dialogHTML.find('.buttonOk').on('click', e => this.closeModalDialog({ name: this.name, description: this.description }));
         this.dialogHTML.find('.buttonCancel').on('click', e => this.closeModalDialog(false));
     }
