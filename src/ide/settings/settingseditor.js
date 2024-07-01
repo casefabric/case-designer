04dbf03bf0a85209dbac830feaadb0fb633f7ab6
Thijs Petter <thijspetter@gmail.com>
2024-07-01 15:05:39 +0200

    Make SettingsEditor fully standalone
    It no longer inherits StandardForm and MovableEditor.
    
    Still can be made cleaner than this, but that is not worth the investment right now.

--- src/ide/settings/settingseditor.js
+++ src/ide/settings/settingseditor.js
@@ -1,25 +1,71 @@
 import IDE from "@ide/ide";
-import StandardForm from "../editors/standardform";
-import SettingsStorage from "./settingsstorage";
 import CodeMirrorConfig from "@util/codemirrorconfig";
+import SettingsStorage from "./settingsstorage";
 
-export default class SettingsEditor extends StandardForm {
-    static asModelEditor(ide) {
-        return {
-                divMovableEditors: ide.html,
-                registerMovableEditor: () => {},
-                selectMovableEditor: () => {},
-                positionMovableEditor: () => {}
-        }
-    }
-
+export default class SettingsEditor {
     /**
      * Editor for the content of the settings of the case model editor
      * @param {IDE} ide 
      */
     constructor(ide) {
-        super(SettingsEditor.asModelEditor(ide), 'Settings (raw JSON)', 'jsoneditor settings-editor');
+        this.htmlParent = ide.html;
         this.ide = ide;
+    }
+
+    get html() {
+        return $(this._html);
+    }
+
+    /**
+     * @param {JQuery<HTMLElement>} html
+     */
+    set html(html) {
+        this._html = $(html);
+    }
+
+    get label() {
+        return this._label;
+    }
+
+    set label(sLabel) {
+        this._label = sLabel;
+        this.html.find('.standardformheader label').html(sLabel);
+    }
+
+    get htmlContainer() {
+        return this._container;
+    }
+
+    renderForm() {
+        if (!this._html) {
+            this.renderHead();
+        }
+        this.renderData();
+    }
+
+    renderHead() {
+        this.html = $(
+            `<div class="basicbox standardform jsoneditor settings-editor">
+                <div class="standardformheader">
+                    <label>Settings (raw JSON)</label>
+                    <div class="button1st_right" title="Close">
+                        <img src="images/close_32.png" />
+                    </div>
+                </div>
+                <div class="standardformcontainer">
+                </div>
+            </div>`
+        );
+        this.htmlParent.append(this.html);
+
+        //add draggable to header
+        this.html.draggable({ handle: '.standardformheader' });
+        this.html.find('label').css('cursor', 'move');
+
+        this.html.resizable();
+        this.html.find('img').on('click', e => this.hide());
+
+        this._container = this.html.find('.standardformcontainer');
     }
 
     renderData() {
@@ -30,11 +76,12 @@ export default class SettingsEditor extends StandardForm {
         }
         const html = $(
             `<div class="btn-group top-bar">
-    <button class="btnResetPreferences">Reset</button>
-    <button class="btnSavePreferences">Save (reload browser to effectuate)</button>
-    <strong><span class="error-message"></span></strong>
-</div>
-<div class="jsoncode"></div>`);
+                <button class="btnResetPreferences">Reset</button>
+                <button class="btnSavePreferences">Save (reload browser to effectuate)</button>
+                <strong><span class="error-message"></span></strong>
+            </div>
+            <div class="jsoncode"></div>`
+        );
         this.errorSpan = html.find('.error-message');
         html.find('.btnResetPreferences').on('click', e => this.reset());
         html.find('.btnSavePreferences').on('click', e => this._save());
@@ -49,6 +96,44 @@ export default class SettingsEditor extends StandardForm {
         // Avoid CTRL-Y and CTRL-Z invoking undo-manager
         //  Apparently, code mirror does not give the event as a parameter to the function, so we work directly on window.event
         this._codeMirrorEditor.on('keydown', e => window.event.stopPropagation());
+    }
+
+    show() {
+        this.visible = true;
+        // mechanism to handle fact that editor is shown
+    }
+
+    hide() {
+        this.visible = false;
+        // mechanism to handle fact that editor is hidden
+    }
+
+    get visible() {
+        return this._visible;
+    }
+
+    /** @param {Boolean} visible */
+    set visible(visible) {
+        this._visible = visible;
+        if (visible) {
+            console.log("Making visible")
+            this.renderForm();
+        }
+        if (this._html) {
+            this.html.css('display', visible ? 'block' : 'none');
+            if (!this._changingVisiblity) {
+                this._changingVisiblity = true;
+                if (visible) {
+                    this.onShow()
+                 }
+                this._changingVisiblity = false;
+            }
+        }
+    }
+
+    delete() {
+        this._visible = false; // Set visible to false to avoid refresh invocations from refreshMovableViews() inside case.js
+        Util.removeHTML(this.html);
     }
 
     /**
