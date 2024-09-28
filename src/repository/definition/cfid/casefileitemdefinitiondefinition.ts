0415c60560e6041b63655de7a084b9fdfb461189
Thijs Petter <thijspetter@gmail.com>
2024-09-28 19:31:40 +0200

    Convert CFID to Typescript

--- src/repository/definition/cfid/casefileitemdefinitiondefinition.ts
+++ src/repository/definition/cfid/casefileitemdefinitiondefinition.ts
@@ -1,32 +1,32 @@
 import { UNSPECIFIED_URI } from "../../../ide/modeleditor/cfid/casefileitemdefinitioneditor";
 import CFIDFile from "../../serverfile/cfidfile";
+import ParameterDefinition from "../cmmn/contract/parameterdefinition";
 import ModelDefinition from "../modeldefinition";
 import PropertyDefinition from "./propertydefinition";
 
 export default class CaseFileDefinitionDefinition extends ModelDefinition {
+    definitionType: string = '';
+    structureRef: string = '';
+    importRef: string = '';
+    properties: PropertyDefinition[] = [];
     /**
      * Imports an XML element and parses it into a in-memory definition structure.
      * @param {CFIDFile} file
      */
-    constructor(file) {
+    constructor(public file: CFIDFile) {
         super(file);
-        this.file = file;
         this.definitionType = this.parseAttribute('definitionType', UNSPECIFIED_URI);
         this.structureRef = this.parseAttribute('structureRef', '');
         this.importRef = this.parseAttribute('importRef', '');
         this.properties = this.parseElements('property', PropertyDefinition)
     }
-    
-    get editor() {
-        return;
-    }
 
-    get inputParameters() {
+    get inputParameters(): ParameterDefinition[] {
         console.warn('Case file has no input/output contract');
         return [];
     }
 
-    get outputParameters() {
+    get outputParameters(): ParameterDefinition[] {
         console.warn('Case file has no input/output contract');
         return [];
     }
