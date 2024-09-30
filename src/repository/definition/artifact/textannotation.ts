f2380c7d0ef6b2a6f6498da29ddc497a1710fa28
Thijs Petter <thijspetter@gmail.com>
2024-09-30 17:55:49 +0200

    Convert /artifact to Typescript

--- src/repository/definition/artifact/textannotation.ts
+++ src/repository/definition/artifact/textannotation.ts
@@ -1,15 +1,19 @@
 import XML from "@util/xml";
 import ArtifactDefinition from "./artifactdefinition";
+import CaseDefinition from "../cmmn/casedefinition";
+import CMMNElementDefinition from "../cmmnelementdefinition";
 
 export default class TextAnnotationDefinition extends ArtifactDefinition {
-    constructor(importNode, caseDefinition, parent) {
+    textFormat: string;
+    text: any;
+    constructor(importNode: Element, public caseDefinition: CaseDefinition, parent: CMMNElementDefinition) {
         super(importNode, caseDefinition, parent);
         this.textFormat = this.parseAttribute('textFormat');
         const textElement = XML.getChildByTagName(this.importNode, 'text');
         this.text = textElement ? XML.getCDATANodeOrSelf(textElement).textContent : '';
     }
 
-    createExportNode(parentNode) {
+    createExportNode(parentNode: Element) {
         super.createExportNode(parentNode, 'textAnnotation', 'textFormat');
 
         const textElement = XML.createChildElement(this.exportNode, 'text');
