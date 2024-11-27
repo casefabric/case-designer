bc49d0fd39282f45bac1fb8915cb5fc07fcca369
Thijs Petter <thijspetter@gmail.com>
2024-11-27 21:03:18 +0100

    Fix setting planning table on discretionary tasks
    Fixes #163

--- src/repository/definition/cmmn/caseplan/planningtabledefinition.ts
+++ src/repository/definition/cmmn/caseplan/planningtabledefinition.ts
@@ -1,3 +1,4 @@
+import ElementDefinition from "@repository/definition/elementdefinition";
 import UnnamedCMMNElementDefinition from "../../unnamedcmmnelementdefinition";
 import CaseDefinition from "../casedefinition";
 import ConstraintDefinition from "./constraintdefinition";
@@ -11,10 +12,25 @@ export default class PlanningTableDefinition extends UnnamedCMMNElementDefinitio
     constructor(importNode: Element, caseDefinition: CaseDefinition, public parent: TaskStageDefinition) {
         super(importNode, caseDefinition, parent);
         this.parent = parent;
-        // @ts-ignore PlanningTables cannot exist inside PlanningTables (as of now)
-        const parentStage: StageDefinition = parent.isTask ? parent.parent : parent;
+
+        // This planning table attached to a HumanTask or in a Stage
+        //  If it is a HumanTask, then the task might be discretionary, 
+        //   and then that task lives inside a PlanningTable to the stage or task around it.
+        // This code recursively tries to find the nearest stage around this planning table
+        const getParentStage = (surrounder: ElementDefinition<CaseDefinition>): StageDefinition => {
+            if (surrounder instanceof StageDefinition) {
+                return surrounder;
+            }
+            if (!surrounder.parent) {
+                // This actually means that our parent is not a task or stage ... should never happen
+                throw new Error('Kindly report this unexpected error')
+            } else {
+                return getParentStage(surrounder.parent);
+            }
+        }
+
         /** @type{Array<PlanItem>} */
-        this.tableItems = parentStage.parseChildren(this);
+        this.tableItems = getParentStage(parent).parseChildren(this);
         this.tableItems.forEach((item: PlanItem) => item.parent = this);
         /** @type{Array<ApplicabilityRuleDefinition>} */
         this.ruleDefinitions = this.parseElements('applicabilityRule', ApplicabilityRuleDefinition);
