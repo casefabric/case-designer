86ae72edd227bc6cfa3fde9d585241726c9010ce
wjgerritsen-0001 <40887209+wjgerritsen-0001@users.noreply.github.com>
2024-12-07 17:04:51 +0100

    Add fixture definition

--- src/repository/definition/testcase/testcasemodeldefinition.ts
+++ src/repository/definition/testcase/testcasemodeldefinition.ts
@@ -1,17 +1,20 @@
 import TestcaseFile from "../../../repository/serverfile/testcasefile";
 import ModelDefinition from "../modeldefinition";
+import FixtureDefinition from "./testfixturedefintion";
 
 export default class TestcaseModelDefinition extends ModelDefinition {
+    fixture: FixtureDefinition;
+
     /**
      * Imports an XML element and parses it into a in-memory definition structure.
      */
     constructor(public file: TestcaseFile) {
         super(file);
+        this.fixture = this.parseElement('fixture', FixtureDefinition) ?? this.createDefinition(FixtureDefinition);
     }
 
-
     toXML() {
-        const xmlDocument = super.exportModel('testcase');
+        const xmlDocument = super.exportModel('testcase', 'fixture');
         return xmlDocument;
     }
 }
