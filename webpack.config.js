72c477c70366da2166c754c890efd3cb2206cf37
wjgerritsen-0001 <40887209+wjgerritsen-0001@users.noreply.github.com>
2025-02-11 09:09:06 +0100

    Convert webpack.config.js to module syntax

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
-const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const HtmlBundlerPlugin = require('html-bundler-webpack-plugin');
-const { BannerPlugin } = require('webpack');
-const nodeExternals = require('webpack-node-externals');
+import CopyWebpackPlugin from 'copy-webpack-plugin';
+import HtmlBundlerPlugin from 'html-bundler-webpack-plugin';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import pkg from 'webpack';
+import nodeExternals from 'webpack-node-externals';
+
+const { BannerPlugin } = pkg;
 const devMode = process.env.DEV_MODE ? process.env.DEV_MODE.trim().toLowerCase() === 'true' : false;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 class BuildPrinter {
     constructor(name) {
@@ -67,7 +73,7 @@ const ideResolvers = {
     },
 };
 
-module.exports = [
+export default [
 { // server
     entry: {
             server: './src/server/startserver.ts'
