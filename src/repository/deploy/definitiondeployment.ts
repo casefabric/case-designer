b2c48ea02c983d433878041ace0056dd2d4bb1bc
Thijs Petter <thijspetter@gmail.com>
2025-04-10 15:41:55 +0200

    Revert "Add method to retrieve all dependencies of a ModelDefinition"
    Restore deployment logic

--- src/repository/deploy/definitiondeployment.ts
+++ src/repository/deploy/definitiondeployment.ts
@@ -8,6 +8,11 @@ export default class DefinitionDeployment {
     constructor(public definitionsDocument: Definitions, public definition: ModelDefinition) {
         definitionsDocument.definitions.push(this);
         this.element = this.definition.toXML().documentElement ?? (() => { throw new Error('No ownerDocument found'); })();
+        definition.elements.map(e => e.externalReferences.all).flat().filter(e => e.nonEmpty).map(e => e.fileName).forEach(fileName => {
+            definition.file.repository.list.filter(file => file.fileName === fileName).filter(file => file.definition).map(file => file.definition).forEach(definition => {
+                definitionsDocument.addDefinition(definition);
+            });
+        });
     }
 
     get fileName() {
