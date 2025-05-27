92fe69bf7591cf0b2ef5b46a986714e07b36d488
wjgerritsen-0001 <40887209+wjgerritsen-0001@users.noreply.github.com>
2025-05-27 18:51:13 +0200

    Simplification in modelselectorcontrol

--- src/ide/editors/modelselectorcontrol.ts
+++ src/ide/editors/modelselectorcontrol.ts
@@ -4,15 +4,12 @@ import ServerFile from "../../repository/serverfile/serverfile";
 import ModelSelectorDialog from "./modelselectordialog";
 
 export default class ModelSelectorControl extends HTMLElement {
+    valueElement?: JQuery<HTMLInputElement>;
     constructor() {
         super();
     }
 
-    async connectedCallback() {
-        await this.render();
-    }
-
-    async render(): Promise<void> {
+    connectedCallback() {
         const modelType = this.attributes.getNamedItem('modeltype')!.value;
         const sourceModelId = this.attributes.getNamedItem('sourcemodel')?.value;
         const selectedModelId = $(this).val() as string | undefined | null ?? undefined;
@@ -20,10 +17,11 @@ export default class ModelSelectorControl extends HTMLElement {
         const selectedModelName = selectedModelId ? window.ide.repository.get(selectedModelId)?.name : '';
 
         const html = $(`<div class="modelselect" style="display: flex; align-items: center; justify-content: space-between;">
-                                <input style='cursor: text; pointer-events: none' disabled id="modelImplementation" type="text" value="${selectedModelName}"></input>
+                                <input class="modelselectvalue" style='cursor: text; pointer-events: none' disabled id="modelImplementation" type="text" value="${selectedModelName}"></input>
                                 <img id="zoomButton" class="zoombt"></img>
                                 <img id="removeButton" class="removeReferenceButton"></img>
                             </div>`);
+        this.valueElement = html.find('.modelselectvalue');
         html.on('click', e => {
             new ModelSelectorDialog(
                 'Select a model to be used as task implementation',
@@ -32,27 +30,27 @@ export default class ModelSelectorControl extends HTMLElement {
                 sourceModelId)
                 .showModalDialog((file: ServerFile<ModelDefinition>) => {
                     if (file) {
-                        this.changeValue(html, file);
+                        this.changeValue(file);
                     }
                 });
         });
         html.find('#removeButton').on('click', e => {
-            this.changeValue(html, undefined);
+            this.changeValue(undefined);
             e.stopPropagation(); // Prevent the click event from propagating to the parent div
         });
 
         // Also make the html a drop target for drag/dropping elements from the repository browser
         html.on('pointerover',
             e => window.ide.repositoryBrowser.setDropHandler(
-                dragData => this.changeValue(html, dragData.file),
+                dragData => this.changeValue(dragData.file),
                 dragData => dragData.file.fileType == modelType));
         html.on('pointerout', e => window.ide.repositoryBrowser.removeDropHandler());
 
         $(this).append(html);
     }
 
-    private changeValue(html: JQuery<HTMLElement>, file?: ServerFile<ModelDefinition>) {
-        html.find('input').val(file?.name ?? '');
+    private changeValue(file?: ServerFile<ModelDefinition>) {
+        this.valueElement!.val(file?.name ?? '');
         $(this).val(file?.fileName ?? '');
         $(this).trigger('change');
     }
