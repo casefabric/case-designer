cafienne/cafienne-ide

Convert views to Typescript (+ remove compatibility)
Also separated sentryview.js into 4 different files (one for each entry-, exit- and reactivatecriterionview)
Also some internal refactoring to definitions.

We can now finally remove the compatibility classes!!!

src/ide/modeleditor/case/elements/caseplanview.ts
@@ -2,21 +2,15 @@
 import ShapeDefinition from "../../../../repository/definition/dimensions/shape";
 import CaseView from "./caseview";
 import CasePlanDecoratorBox from "./decorator/box/caseplandecoratorbox";
+import ExitCriterionView from "./exitcriterionview";
 import CasePlanHalo from "./halo/cmmn/caseplanhalo";
 import CasePlanProperties from "./properties/caseplanproperties";
-import { ExitCriterionView } from "./sentryview";
 import StageView from "./stageview";
 
 const CPM_TAB_HEIGHT = 22;
 
-export default class CasePlanView extends StageView {
-    /**
-     * 
-     * @param {CaseView} cs 
-     * @param {*} x 
-     * @param {*} y 
-     */
-    static create(cs, x = 10, y = 10) {
+export default class CasePlanView extends StageView<CasePlanDefinition> {
+    static createNew(cs: CaseView, x = 10, y = 10) {
         const definition = cs.caseDefinition.casePlan;
         const shape = cs.diagram.createShape(x, y, 800, 500, definition.id);
         return new CasePlanView(cs, definition, shape)
@@ -24,16 +18,13 @@ export default class CasePlanView extends StageView {
 
     /**
      * Creates a new CasePlan model
-     * @param {CaseView} cs Must be the CaseView object itself.
-     * @param {CasePlanDefinition} definition 
-     * @param {ShapeDefinition} shape 
-     */
-    constructor(cs, definition, shape) {
+      */
+    constructor(cs: CaseView, definition: CasePlanDefinition, shape: ShapeDefinition) {
         super(cs, undefined, definition, shape);
         this.definition = definition;
     }
 
-    referencesDefinitionElement(definitionId) {
+    referencesDefinitionElement(definitionId: string) {
         // Check whether the case parameters may be using the case file item
         if (this.case.caseDefinition.input.find(p => p.bindingRef.references(definitionId))) {
             return true;
@@ -58,9 +49,8 @@ export default class CasePlanView extends StageView {
 
     /**
      * Show or hide the halo and resizer
-     * @param {Boolean} show 
      */
-    __renderBoundary(show) {
+    __renderBoundary(show: boolean) {
         if (this.case.selectedElement === this) {
             this.resizer.visible = true;
         } else {
@@ -77,7 +67,7 @@ export default class CasePlanView extends StageView {
         return { x: 280, y: 13 };
     }
 
-    surrounds(element) {
+    surrounds(element: CasePlanView) {
         // Avoid the StageView.acquireChildren method to throw out elements outside the case plan (even though visually they can be dragged outside)
         return element != this;
     }
@@ -105,10 +95,8 @@ export default class CasePlanView extends StageView {
 
     /**
      * Override of basic resize method.
-     * @param {*} w 
-     * @param {*} h 
      */
-    resizing(w, h) {
+    resizing(w: number, h: number) {
         super.resizing(w, h);
         // The rect must also be given some new dimensions
         this.html.find('.cmmn-border').attr('width', this.shape.width);
@@ -120,11 +108,11 @@ export default class CasePlanView extends StageView {
         delete this.case.casePlanModel;
     }
 
-    canHaveCriterion(criterionType) {
+    canHaveCriterion(criterionType: any) {
         return criterionType == ExitCriterionView;
     }
 
-    createCMMNChild(viewType, x, y) {
+    createCMMNChild(viewType: any, x: number, y: number) {
         if (viewType == ExitCriterionView) {
             return this.__addCMMNChild(ExitCriterionView.create(this, x, y));
         } else {

src/ide/modeleditor/case/elements/milestoneview.ts
@@ -1,19 +1,16 @@
 ﻿import MilestoneDefinition from "../../../../repository/definition/cmmn/caseplan/milestonedefinition";
 import ShapeDefinition from "../../../../repository/definition/dimensions/shape";
 import MilestoneDecoratorBox from "./decorator/box/milestonedecoratorbox";
+import EntryCriterionView from "./entrycriterionview";
 import PlanItemView from "./planitemview";
 import MilestoneProperties from "./properties/milestoneproperties";
-import { EntryCriterionView } from "./sentryview";
 import StageView from "./stageview";
 
-export default class MilestoneView extends PlanItemView {
+export default class MilestoneView extends PlanItemView<MilestoneDefinition> {
     /**
-     * 
-     * @param {StageView} stage 
-     * @param {*} x 
-     * @param {*} y 
+     * Create a new MilestoneView at the given coordinates.
      */
-    static create(stage, x, y) {
+    static create(stage: StageView, x: number, y: number): MilestoneView {
         const definition = stage.definition.createPlanItem(MilestoneDefinition);
         const shape = stage.case.diagram.createShape(x, y, 100, 40, definition.id);
         return new MilestoneView(stage, definition, shape);
@@ -21,13 +18,9 @@ export default class MilestoneView extends PlanItemView {
 
     /**
      * Creates a new MilestoneView element.
-     * @param {StageView} parent 
-     * @param {MilestoneDefinition} definition
-     * @param {ShapeDefinition} shape 
      */
-    constructor(parent, definition, shape) {
+    constructor(public parent: StageView, definition: MilestoneDefinition, shape: ShapeDefinition) {
         super(parent.case, parent, definition, shape);
-        this.definition = definition;
     }
 
     get wrapText() {
@@ -63,19 +56,16 @@ export default class MilestoneView extends PlanItemView {
     }
 
     /**
-     * returns true when an element of type 'elementType' can be added as a child to this element
-     * @param {Function} elementType 
+     * Returns true when an element of type 'elementType' can be added as a child to this element
      */
-    __canHaveAsChild(elementType) {
+    __canHaveAsChild(elementType: any) {
         return this.canHaveCriterion(elementType);
     }
 
     /**
-     * 
-     * @param {Function} criterionType 
-     * @returns 
+     * Returns true if the criterionType can be added to this milestone
      */
-    canHaveCriterion(criterionType) {
+    canHaveCriterion(criterionType: any) {
         return criterionType == EntryCriterionView;
     }
 

src/ide/modeleditor/case/elements/planningtableview.ts
@@ -5,34 +5,27 @@ import Images from "../../../util/images/images";
 import CMMNElementView from "./cmmnelementview";
 import PlanningTableHalo from "./halo/cmmn/planningtablehalo";
 import PlanningTableProperties from "./properties/planningtableproperties";
+import StageView from "./stageview";
 import TaskStageView from "./taskstageview";
 
-export default class PlanningTableView extends CMMNElementView {
+export default class PlanningTableView extends CMMNElementView<PlanningTableDefinition> {
+    stage: TaskStageView;
 
-    /**
-     * 
-     * @param {TaskStageView} parent 
-     * @param {PlanningTableDefinition} definition 
-     * @param {ShapeDefinition} shape 
-     */
-    constructor(parent, definition, shape) {
+    constructor(public parent: TaskStageView, definition: PlanningTableDefinition, shape: ShapeDefinition) {
         super(parent.case, parent, definition, shape);
-        // Setters enable better type introspection
-        this.definition = definition;
         this.parent = parent;
         this.__resizable = false;
         parent.__addCMMNChild(this);
 
-        this.stage = this.parent.isStage ? this.parent : this.parent.parent;
+        this.stage = this.parent.isStage ? this.parent as StageView : this.parent.parent as StageView;
         // Now also render the discretionary items from the definition in our parent
         this.definition.tableItems.forEach(item => this.parent.addDiscretionaryItem(item));
     }
 
     /**
      * Override select in both planningtable and sentry to immediately show properties.
-     * @param {Boolean} selected 
      */
-    __select(selected) {
+    __select(selected: boolean) {
         super.__select(selected);
         if (selected) {
             this.propertiesView.show();
@@ -56,43 +49,43 @@ export default class PlanningTableView extends CMMNElementView {
      * Deleting the planning table should also inform our parent that we're gone...
      */
     __delete() {
-        // First make all discretionary items non-discretionary, in order not to loose their drawings.
+        // First make all discretionary items non-discretionary, in order not to lose their drawings.
         this.definition.tableItems.map(i => i).forEach(item => item.switchType());
         // Invoke super logic, but only after switching type of our discretionary items,
-        //  otherwise the pointers are lost since super.__delete() removes the definition.
+        // otherwise the pointers are lost since super.__delete() removes the definition.
         super.__delete();
         // Render the stage again, in order to remove dotted lines from the converted former discretionary items
         this.stage.refreshView();
     }
 
-    moved(x, y, newParent) {
+    moved(x: number, y: number, newParent: any) {
         this.moving(x, y);
     }
 
     /**
-     * A planningTable has a fixed position on it's parent, it cannot be moved.
+     * A planningTable has a fixed position on its parent, it cannot be moved.
      * Position cursor is not relevant
      */
-    moving(x, y) {
+    moving(x: number, y: number) {
         const parentX = this.parent.shape.x;
         const parentY = this.parent.shape.y;
-        //create a point relative to the parentElement, where the planningTable must be positioned relative to the parent
-        const point = g.point(parentX + this.parent.__planningTablePosition.x, parentY + this.parent.__planningTablePosition.y);
+        // create a point relative to the parentElement, where the planningTable must be positioned relative to the parent
+        const point = new g.Point(parentX + this.parent.__planningTablePosition.x, parentY + this.parent.__planningTablePosition.y);
 
         // get the absolute position of the planningTable
-        //  NOTE: Planning Table does NOT yet store a CMMNShape object. It would be better if it did... Now we have to use joint :(
+        // NOTE: Planning Table does NOT yet store a CMMNShape object. It would be better if it did... Now we have to use joint :(
         const ptX = this.shape.x;
         const ptY = this.shape.y;
 
-        //position planningTable with respect to the parent
+        // position planningTable with respect to the parent
         const translateX = point.x - ptX;
         const translateY = point.y - ptY;
 
         this.xyz_joint.translate(translateX, translateY);
     }
 
-    referencesDefinitionElement(definitionId) {
-        // check in applicabilty rules; note: we're checking sourceRef, but it ought to be contextRef...
+    referencesDefinitionElement(definitionId: string) {
+        // check in applicability rules; note: we're checking sourceRef, but it ought to be contextRef...
         if (this.definition.ruleDefinitions.find(rule => rule.sourceRef.references(definitionId))) {
             return true;
         }

casefabric/case-designer

src/ide/modeleditor/case/elements/caseplanview.ts
@@ -2,21 +2,15 @@
 import ShapeDefinition from "../../../../repository/definition/dimensions/shape";
 import CaseView from "./caseview";
 import CasePlanDecoratorBox from "./decorator/box/caseplandecoratorbox";
+import ExitCriterionView from "./exitcriterionview";
 import CasePlanHalo from "./halo/cmmn/caseplanhalo";
 import CasePlanProperties from "./properties/caseplanproperties";
-import { ExitCriterionView } from "./sentryview";
 import StageView from "./stageview";
 
 const CPM_TAB_HEIGHT = 22;
 
-export default class CasePlanView extends StageView {
-    /**
-     * 
-     * @param {CaseView} cs 
-     * @param {*} x 
-     * @param {*} y 
-     */
-    static create(cs, x = 10, y = 10) {
+export default class CasePlanView extends StageView<CasePlanDefinition> {
+    static createNew(cs: CaseView, x = 10, y = 10) {
         const definition = cs.caseDefinition.casePlan;
         const shape = cs.diagram.createShape(x, y, 800, 500, definition.id);
         return new CasePlanView(cs, definition, shape)
@@ -24,16 +18,13 @@ export default class CasePlanView extends StageView {
 
     /**
      * Creates a new CasePlan model
-     * @param {CaseView} cs Must be the CaseView object itself.
-     * @param {CasePlanDefinition} definition 
-     * @param {ShapeDefinition} shape 
-     */
-    constructor(cs, definition, shape) {
+      */
+    constructor(cs: CaseView, definition: CasePlanDefinition, shape: ShapeDefinition) {
         super(cs, undefined, definition, shape);
         this.definition = definition;
     }
 
-    referencesDefinitionElement(definitionId) {
+    referencesDefinitionElement(definitionId: string) {
         // Check whether the case parameters may be using the case file item
         if (this.case.caseDefinition.input.find(p => p.bindingRef.references(definitionId))) {
             return true;
@@ -58,9 +49,8 @@ export default class CasePlanView extends StageView {
 
     /**
      * Show or hide the halo and resizer
-     * @param {Boolean} show 
      */
-    __renderBoundary(show) {
+    __renderBoundary(show: boolean) {
         if (this.case.selectedElement === this) {
             this.resizer.visible = true;
         } else {
@@ -77,7 +67,7 @@ export default class CasePlanView extends StageView {
         return { x: 280, y: 13 };
     }
 
-    surrounds(element) {
+    surrounds(element: CasePlanView) {
         // Avoid the StageView.acquireChildren method to throw out elements outside the case plan (even though visually they can be dragged outside)
         return element != this;
     }
@@ -105,10 +95,8 @@ export default class CasePlanView extends StageView {
 
     /**
      * Override of basic resize method.
-     * @param {*} w 
-     * @param {*} h 
      */
-    resizing(w, h) {
+    resizing(w: number, h: number) {
         super.resizing(w, h);
         // The rect must also be given some new dimensions
         this.html.find('.cmmn-border').attr('width', this.shape.width);
@@ -120,11 +108,11 @@ export default class CasePlanView extends StageView {
         delete this.case.casePlanModel;
     }
 
-    canHaveCriterion(criterionType) {
+    canHaveCriterion(criterionType: Function) {
         return criterionType == ExitCriterionView;
     }
 
-    createCMMNChild(viewType, x, y) {
+    createCMMNChild(viewType: Function, x: number, y: number) {
         if (viewType == ExitCriterionView) {
             return this.__addCMMNChild(ExitCriterionView.create(this, x, y));
         } else {

src/ide/modeleditor/case/elements/milestoneview.ts
@@ -1,19 +1,16 @@
 ﻿import MilestoneDefinition from "../../../../repository/definition/cmmn/caseplan/milestonedefinition";
 import ShapeDefinition from "../../../../repository/definition/dimensions/shape";
 import MilestoneDecoratorBox from "./decorator/box/milestonedecoratorbox";
+import EntryCriterionView from "./entrycriterionview";
 import PlanItemView from "./planitemview";
 import MilestoneProperties from "./properties/milestoneproperties";
-import { EntryCriterionView } from "./sentryview";
 import StageView from "./stageview";
 
-export default class MilestoneView extends PlanItemView {
+export default class MilestoneView extends PlanItemView<MilestoneDefinition> {
     /**
-     * 
-     * @param {StageView} stage 
-     * @param {*} x 
-     * @param {*} y 
+     * Create a new MilestoneView at the given coordinates.
      */
-    static create(stage, x, y) {
+    static create(stage: StageView, x: number, y: number): MilestoneView {
         const definition = stage.definition.createPlanItem(MilestoneDefinition);
         const shape = stage.case.diagram.createShape(x, y, 100, 40, definition.id);
         return new MilestoneView(stage, definition, shape);
@@ -21,13 +18,9 @@ export default class MilestoneView extends PlanItemView {
 
     /**
      * Creates a new MilestoneView element.
-     * @param {StageView} parent 
-     * @param {MilestoneDefinition} definition
-     * @param {ShapeDefinition} shape 
      */
-    constructor(parent, definition, shape) {
+    constructor(public parent: StageView, definition: MilestoneDefinition, shape: ShapeDefinition) {
         super(parent.case, parent, definition, shape);
-        this.definition = definition;
     }
 
     get wrapText() {
@@ -63,19 +56,16 @@ export default class MilestoneView extends PlanItemView {
     }
 
     /**
-     * returns true when an element of type 'elementType' can be added as a child to this element
-     * @param {Function} elementType 
+     * Returns true when an element of type 'elementType' can be added as a child to this element
      */
-    __canHaveAsChild(elementType) {
+    __canHaveAsChild(elementType: Function) {
         return this.canHaveCriterion(elementType);
     }
 
     /**
-     * 
-     * @param {Function} criterionType 
-     * @returns 
+     * Returns true if the criterionType can be added to this milestone
      */
-    canHaveCriterion(criterionType) {
+    canHaveCriterion(criterionType: Function) {
         return criterionType == EntryCriterionView;
     }
 

src/ide/modeleditor/case/elements/planningtableview.ts
@@ -5,34 +5,27 @@ import Images from "../../../util/images/images";
 import CMMNElementView from "./cmmnelementview";
 import PlanningTableHalo from "./halo/cmmn/planningtablehalo";
 import PlanningTableProperties from "./properties/planningtableproperties";
+import StageView from "./stageview";
 import TaskStageView from "./taskstageview";
 
-export default class PlanningTableView extends CMMNElementView {
+export default class PlanningTableView extends CMMNElementView<PlanningTableDefinition> {
+    stage: TaskStageView;
 
-    /**
-     * 
-     * @param {TaskStageView} parent 
-     * @param {PlanningTableDefinition} definition 
-     * @param {ShapeDefinition} shape 
-     */
-    constructor(parent, definition, shape) {
+    constructor(public parent: TaskStageView, definition: PlanningTableDefinition, shape: ShapeDefinition) {
         super(parent.case, parent, definition, shape);
-        // Setters enable better type introspection
-        this.definition = definition;
         this.parent = parent;
         this.__resizable = false;
         parent.__addCMMNChild(this);
 
-        this.stage = this.parent.isStage ? this.parent : this.parent.parent;
+        this.stage = this.parent.isStage ? this.parent as StageView : this.parent.parent as StageView;
         // Now also render the discretionary items from the definition in our parent
         this.definition.tableItems.forEach(item => this.parent.addDiscretionaryItem(item));
     }
 
     /**
      * Override select in both planningtable and sentry to immediately show properties.
-     * @param {Boolean} selected 
      */
-    __select(selected) {
+    __select(selected: boolean) {
         super.__select(selected);
         if (selected) {
             this.propertiesView.show();
@@ -56,43 +49,43 @@ export default class PlanningTableView extends CMMNElementView {
      * Deleting the planning table should also inform our parent that we're gone...
      */
     __delete() {
-        // First make all discretionary items non-discretionary, in order not to loose their drawings.
+        // First make all discretionary items non-discretionary, in order not to lose their drawings.
         this.definition.tableItems.map(i => i).forEach(item => item.switchType());
         // Invoke super logic, but only after switching type of our discretionary items,
-        //  otherwise the pointers are lost since super.__delete() removes the definition.
+        // otherwise the pointers are lost since super.__delete() removes the definition.
         super.__delete();
         // Render the stage again, in order to remove dotted lines from the converted former discretionary items
         this.stage.refreshView();
     }
 
-    moved(x, y, newParent) {
+    moved(x: number, y: number, newParent: CMMNElementView) {
         this.moving(x, y);
     }
 
     /**
-     * A planningTable has a fixed position on it's parent, it cannot be moved.
+     * A planningTable has a fixed position on its parent, it cannot be moved.
      * Position cursor is not relevant
      */
-    moving(x, y) {
+    moving(x: number, y: number) {
         const parentX = this.parent.shape.x;
         const parentY = this.parent.shape.y;
-        //create a point relative to the parentElement, where the planningTable must be positioned relative to the parent
-        const point = g.point(parentX + this.parent.__planningTablePosition.x, parentY + this.parent.__planningTablePosition.y);
+        // create a point relative to the parentElement, where the planningTable must be positioned relative to the parent
+        const point = new g.Point(parentX + this.parent.__planningTablePosition.x, parentY + this.parent.__planningTablePosition.y);
 
         // get the absolute position of the planningTable
-        //  NOTE: Planning Table does NOT yet store a CMMNShape object. It would be better if it did... Now we have to use joint :(
+        // NOTE: Planning Table does NOT yet store a CMMNShape object. It would be better if it did... Now we have to use joint :(
         const ptX = this.shape.x;
         const ptY = this.shape.y;
 
-        //position planningTable with respect to the parent
+        // position planningTable with respect to the parent
         const translateX = point.x - ptX;
         const translateY = point.y - ptY;
 
         this.xyz_joint.translate(translateX, translateY);
     }
 
-    referencesDefinitionElement(definitionId) {
-        // check in applicabilty rules; note: we're checking sourceRef, but it ought to be contextRef...
+    referencesDefinitionElement(definitionId: string) {
+        // check in applicability rules; note: we're checking sourceRef, but it ought to be contextRef...
         if (this.definition.ruleDefinitions.find(rule => rule.sourceRef.references(definitionId))) {
             return true;
         }