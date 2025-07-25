1cf7231697be38b804000c8902924171bf37b0a7
wjgerritsen-0001 <40887209+wjgerritsen-0001@users.noreply.github.com>
2025-07-25 12:08:19 +0200

    Fail on javascript warnings or errors

---
+++ test/integration/extensions/verifybrowserconsole.ts
@@ -0,0 +1,24 @@
+type logEvent = {
+    level: string;
+    message: string;
+    timestamp: string;
+}
+
+// filter out those warnings
+const ignoreEvents = [
+    /: no source content to parse/, // due to parallel execution;
+    /Still using [^\s]* \?\?\?  Better not, since it no longer exists in the server .../, // due to parallel execution
+    /Cannot add renderer again found/, // due to issues in TypeEditor
+    /Updating the task name to/, // not a real warning, should be info?
+]
+
+export const mochaHooks = {
+    afterEach: async function () {
+        this.test.title = `Verify browser console logs for : ${this.currentTest.title}`;
+
+        const logs = (<logEvent[]>await browser.getLogs('browser'))
+            .filter(e => !ignoreEvents.some(m => e.message.match(m)));
+
+        await expect(logs).toStrictEqual([]);
+    }
+};
