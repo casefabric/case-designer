aef0a7db351bc959dc32e2ef5a9f7613e97c8450
wjgerritsen-0001 <40887209+wjgerritsen-0001@users.noreply.github.com>
2025-07-25 12:08:19 +0200

    Fail on javascript warnings or errors

---
+++ test/integration/extensions/verifybrowserconsole.ts
@@ -0,0 +1,27 @@
+type logEvent = {
+    level: string;
+    message: string;
+    timestamp: string;
+}
+
+// filter out those warnings
+const ignoreEvents = [
+    /Form submission canceled because the form is not connected/, // filter out those messages triggered by faulty button
+    /: no source content to parse/, // due to parallel execution;
+    /Still using [^\s]* \?\?\?  Better not, since it no longer exists in the server .../, // due to parallel execution
+    /Cannot add renderer again found/, // due to issues in TypeEditor
+    /Updating the task name to/, // not a real warning, should be info?
+    /CaseTeam: Did not receive a file for [^\s]*.caseteam/, // warning due to problem in caseteam editor with remove of team
+    /Still using [^\s]*.caseteam \?\?\?  Better not, since it no longer exists in the server .../, // same as above
+]
+
+export const mochaHooks = {
+    afterEach: async function () {
+        this.test.title = `Verify browser console logs for : ${this.currentTest.title}`;
+
+        const logs = (<logEvent[]>await browser.getLogs('browser'))
+            .filter(e => !ignoreEvents.some(m => e.message.match(m)));
+
+        await expect(logs).toStrictEqual([]);
+    }
+};
