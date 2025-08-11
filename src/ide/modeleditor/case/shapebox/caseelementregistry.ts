e0ff91abfb852576309903c1cbdac05b6460307d
wjgerritsen-0001 <40887209+wjgerritsen-0001@users.noreply.github.com>
2025-08-11 17:14:30 +0200

    Move Shapebox to generic modelcanvas

--- src/ide/modeleditor/case/shapebox/caseelementregistry.ts
+++ src/ide/modeleditor/case/shapebox/caseelementregistry.ts
@@ -1,4 +1,4 @@
-
+import ElementRegistry from "../../../editors/modelcanvas/shapebox/elementregistry";
 import Icons from "../../../util/images/icons";
 import Shapes from "../../../util/images/shapes";
 import CaseFileItemView from "../elements/casefileitemview";
@@ -16,14 +16,10 @@ import TextAnnotationView from "../elements/textannotationview";
 import TimerEventView from "../elements/timereventview";
 import UserEventView from "../elements/usereventview";
 
-export default class ElementRegistry {
-    static viewMetadata: ElementMetadata[] = [];
+export default class CaseElementRegistry extends ElementRegistry {
+    constructor() {
+        super();
 
-    static initialize() {
-        if (this.viewMetadata.length > 0) {
-            // Means we already initialized.
-            return;
-        }
         this.registerType(HumanTaskView, 'Human Task', Shapes.HumanTask, Icons.HumanTask);
         this.registerType(CaseTaskView, 'Case Task', Shapes.CaseTask, Icons.CaseTask);
         this.registerType(ProcessTaskView, 'Process Task', Shapes.ProcessTask, Icons.ProcessTask);
@@ -38,37 +34,5 @@ export default class ElementRegistry {
         this.registerType(CaseFileItemView, 'Case File Item', Shapes.CaseFileItem);
         this.registerType(TextAnnotationView, 'Text Annotation', Shapes.TextAnnotation);
         this.registerType(PlanningTableView, 'Planning Table');
-    }
-
-    /**
-     * Registers a class that extends CaseElementView by it's name.
-     * @param cmmnElementType 
-     * @param typeDescription Friendly description of the type
-     * @param smallImageURL url of small image (for drag/drop, shapebox, etc.)
-     * @param menuImageURL optional url of image shown in repository browser
-     */
-    static registerType(cmmnElementType: Function, typeDescription: string, smallImageURL: string = '', menuImageURL: string = smallImageURL) {
-        this.viewMetadata.push(new ElementMetadata(cmmnElementType, typeDescription, smallImageURL, menuImageURL));
-    }
-
-    static getType(name: string) {
-        return this.viewMetadata.find(type => type.name === name)?.cmmnElementType;
-    }
-}
-
-export class ElementMetadata {
-    name: string;
-
-    constructor(public cmmnElementType: Function, public typeDescription: string, public smallImage: string, public menuImage: string) {
-        this.name = cmmnElementType.name;
-
-        // TODO: Remove backwards compatibility code. Still used in e.g. modellistpanel 
-        (<any>cmmnElementType).typeDescription = typeDescription;
-        (<any>cmmnElementType).smallImage = smallImage;
-        (<any>cmmnElementType).menuImage = menuImage;
-    }
-
-    get hasImage() {
-        return this.smallImage !== '';
     }
 }
