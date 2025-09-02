cafienne/cafienne-ide

Connect testcase harness with typescript client lib

src/testharness/runner/runner.ts
@@ -1,28 +1,73 @@
+import { Config as CafienneClientConfig, PlatformService, Tenant, TenantUser, User } from "@cafienne/typescript-client";
 import CaseDefinition from "../../repository/definition/cmmn/casedefinition";
+import TestcaseModelDefinition from "../../repository/definition/testcase/testcasemodeldefinition";
 import Definitions from "../../repository/deploy/definitions";
 import Repository from "../../repository/repository";
- 
-export class Runner {
+
+export default class Runner {
     repository: Repository;
 
     constructor(repository: Repository) {
         console.log("Runner constructor");
         this.repository = repository;
+
+        // TODO: Fetch this from Settings in IDE (or as constructor arguments)
+        // Set the configuration for the Cafienne client
+        CafienneClientConfig.CafienneService.url = 'http://localhost:33027/';
+        CafienneClientConfig.TokenService.url = 'http://localhost:33077/token';
+        CafienneClientConfig.TokenService.issuer='http://localhost:33077'
     }
 
-    async runTestForCase(caseDefinition: CaseDefinition): Promise<string> {
+    async runTests(filePattern: string): Promise<string> {
+        console.group("Test runner");
+        console.log(`Running tests for ${filePattern}`);
+        const testcases = this.repository.getTestcases()
+            .filter(test => test.fileName.match(filePattern))
+            .map(testcase => testcase.definition)
+            .filter(testcase => testcase !== undefined) as TestcaseModelDefinition[];
+
+        await this.runTestcases(testcases);
+
+        console.groupEnd();
+        return "success";
+    }
+
+    async runTestsForCase(caseDefinition: CaseDefinition): Promise<string> {
         console.group(`Test runner: ${caseDefinition.name}` );
         const allRelatedDefinitions = new Definitions(caseDefinition);
         const compiledCase = allRelatedDefinitions.contents();
 
         const relevantTestCases = this.repository.getTestcases()
-            .forEach(testcase => {
-                console.log(`Running testcase: ${testcase.name}`);
-            });
+            .map(testcase => testcase.definition)
+            .filter(testcase => testcase !== undefined) as TestcaseModelDefinition[];
+        
+        await this.runTestcases(relevantTestCases);
             
         console.groupEnd();
 
         return "success";
+    }
+
+    private async runTestcases(testcases: TestcaseModelDefinition[]): Promise<void> {
+        await Promise.all(testcases.map(x => this.runTestcase(x)));
+    }
+
+    private async runTestcase(testcase: TestcaseModelDefinition): Promise<void> {
+        console.log(`Running testcase: ${testcase.name}`);
+
+        const adminUser = await new User("admin").login();
+        console.log(`user logged in: ${adminUser}`);
+
+        const tenant = await this.createTenant(adminUser);
+    }
+
+    private async createTenant(adminUser: User): Promise<Tenant | undefined> {
+        const tenantOwner = new TenantUser(adminUser.id);
+        tenantOwner.isOwner = true;
+        const tenant = new Tenant("Test-tenant-" + Date.now().toFixed(), [tenantOwner]);
+        await PlatformService.createTenant(adminUser, tenant, 204);
+
+        return tenant;
     }
 }
 

webpack.config.js
@@ -80,6 +80,7 @@ module.exports = [
 { // repository cli
     entry: {
         transpile: { import: './src/repository/transpile.ts', dependOn: ['shared'] },
+        testrunner: { import: './src/testharness/runnermain.ts', dependOn: ['shared'] },
         shared: './src/index.js',
         index: { import: './src/index.js', dependOn: ['shared'] },
     },
@@ -110,11 +111,13 @@ module.exports = [
     experiments: {
         outputModule: true,
     },
+    externalsType: "module-import",
     externals: [
-        nodeExternals({ importType: (request) => `import ${request}` }),
         {
-            'fs': 'import fs',
-        }
+            'node-fetch': 'node-commonjs node-fetch', // https://github.com/matthew-andrews/isomorphic-fetch/issues/194#issuecomment-1513787724
+            '@cafienne/typescript-client': 'import @cafienne/typescript-client',
+        },
+        nodeExternals({ importType: (request) => `import ${request}` }),
     ],
 },
 { // ide

casefabric/case-designer

src/testharness/runner/runner.ts
@@ -1,28 +1,73 @@
+import { Config as CafienneClientConfig, PlatformService, Tenant, TenantUser, User } from "@casefabric/typescript-client";
 import CaseDefinition from "../../repository/definition/cmmn/casedefinition";
+import TestcaseModelDefinition from "../../repository/definition/testcase/testcasemodeldefinition";
 import Definitions from "../../repository/deploy/definitions";
 import Repository from "../../repository/repository";
- 
-export class Runner {
+
+export default class Runner {
     repository: Repository;
 
     constructor(repository: Repository) {
         console.log("Runner constructor");
         this.repository = repository;
+
+        // TODO: Fetch this from Settings in IDE (or as constructor arguments)
+        // Set the configuration for the Cafienne client
+        CafienneClientConfig.CafienneService.url = 'http://localhost:33027/';
+        CafienneClientConfig.TokenService.url = 'http://localhost:33077/token';
+        CafienneClientConfig.TokenService.issuer = 'http://localhost:33077'
     }
 
-    async runTestForCase(caseDefinition: CaseDefinition): Promise<string> {
-        console.group(`Test runner: ${caseDefinition.name}` );
+    async runTests(filePattern: string): Promise<string> {
+        console.group("Test runner");
+        console.log(`Running tests for ${filePattern}`);
+        const testcases = this.repository.getTestcases()
+            .filter(test => test.fileName.match(filePattern))
+            .map(testcase => testcase.definition)
+            .filter(testcase => testcase !== undefined) as TestcaseModelDefinition[];
+
+        await this.runTestcases(testcases);
+
+        console.groupEnd();
+        return "success";
+    }
+
+    async runTestsForCase(caseDefinition: CaseDefinition): Promise<string> {
+        console.group(`Test runner: ${caseDefinition.name}`);
         const allRelatedDefinitions = new Definitions(caseDefinition);
         const compiledCase = allRelatedDefinitions.contents();
 
         const relevantTestCases = this.repository.getTestcases()
-            .forEach(testcase => {
-                console.log(`Running testcase: ${testcase.name}`);
-            });
-            
+            .map(testcase => testcase.definition)
+            .filter(testcase => testcase !== undefined) as TestcaseModelDefinition[];
+
+        await this.runTestcases(relevantTestCases);
+
         console.groupEnd();
 
         return "success";
+    }
+
+    private async runTestcases(testcases: TestcaseModelDefinition[]): Promise<void> {
+        await Promise.all(testcases.map(x => this.runTestcase(x)));
+    }
+
+    private async runTestcase(testcase: TestcaseModelDefinition): Promise<void> {
+        console.log(`Running testcase: ${testcase.name}`);
+
+        const adminUser = await new User("admin").login();
+        console.log(`user logged in: ${adminUser}`);
+
+        const tenant = await this.createTenant(adminUser);
+    }
+
+    private async createTenant(adminUser: User): Promise<Tenant | undefined> {
+        const tenantOwner = new TenantUser(adminUser.id);
+        tenantOwner.isOwner = true;
+        const tenant = new Tenant("Test-tenant-" + Date.now().toFixed(), [tenantOwner]);
+        await PlatformService.createTenant(adminUser, tenant, 204);
+
+        return tenant;
     }
 }
 

webpack.config.js
@@ -92,6 +92,7 @@ module.exports = [
 { // repository cli
     entry: {
         compile: { import: './src/repository/compile.ts', dependOn: ['shared'] },
+        testrunner: { import: './src/testharness/runnermain.ts', dependOn: ['shared'] },
         shared: './src/index.js',
         index: { import: './src/index.js', dependOn: ['shared'] },
     },
@@ -122,11 +123,13 @@ module.exports = [
     experiments: {
         outputModule: true,
     },
+    externalsType: "module-import",
     externals: [
-        nodeExternals({ importType: (request) => `import ${request}` }),
         {
-            'fs': 'import fs',
-        }
+            'node-fetch': 'node-commonjs node-fetch', // https://github.com/matthew-andrews/isomorphic-fetch/issues/194#issuecomment-1513787724
+            '@casefabric/typescript-client': 'import @casefabric/typescript-client',
+        },
+        nodeExternals({ importType: (request) => `import ${request}` }),
     ],
 },
 { // ide