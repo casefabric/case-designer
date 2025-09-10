cafienne/cafienne-ide

Connect testcase harness with typescript client lib

webpack.config.js
@@ -80,6 +80,7 @@ module.exports = [
 { // repository cli
     entry: {
         transpile: { import: './src/repository/transpile.ts', dependOn: ['shared'] },
+        testrunner: { import: './src/testharness/runnermain.ts', dependOn: ['shared'] },
         shared: './src/index.js',
         index: { import: './src/index.js', dependOn: ['shared'] },
     },
@@ -110,11 +111,13 @@ module.exports = [
     experiments: {
         outputModule: true,
     },
+    externalsType: "module-import",
     externals: [
-        nodeExternals({ importType: (request) => `import ${request}` }),
         {
-            'fs': 'import fs',
-        }
+            'node-fetch': 'node-commonjs node-fetch', // https://github.com/matthew-andrews/isomorphic-fetch/issues/194#issuecomment-1513787724
+            '@cafienne/typescript-client': 'import @cafienne/typescript-client',
+        },
+        nodeExternals({ importType: (request) => `import ${request}` }),
     ],
 },
 { // ide

casefabric/case-designer

webpack.config.js
@@ -98,6 +98,7 @@ export default [
 { // repository cli
     entry: {
         compile: { import: './src/repository/compile.ts', dependOn: ['shared'] },
+        testrunner: { import: './src/testharness/runnermain.ts', dependOn: ['shared'] },
         shared: './src/index.js',
         index: { import: './src/index.js', dependOn: ['shared'] },
     },
@@ -128,11 +129,13 @@ export default [
     experiments: {
         outputModule: true,
     },
+    externalsType: "module-import",
     externals: [
-        nodeExternals({ importType: (request) => `import ${request}` }),
         {
-            'fs': 'import fs',
-        }
+            'node-fetch': 'node-commonjs node-fetch', // https://github.com/matthew-andrews/isomorphic-fetch/issues/194#issuecomment-1513787724
+            '@casefabric/typescript-client': 'import @casefabric/typescript-client',
+        },
+        nodeExternals({ importType: (request) => `import ${request}` }),
     ],
 },
 { // ide