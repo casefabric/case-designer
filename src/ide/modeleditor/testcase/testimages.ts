97aff2ea6ce484cc78a4bae001abdd70c24ec02e
wjgerritsen-0001 <40887209+wjgerritsen-0001@users.noreply.github.com>
2025-09-15 10:50:11 +0200

    Add variant view

--- src/ide/modeleditor/testcase/testimages.ts
+++ src/ide/modeleditor/testcase/testimages.ts
@@ -1,9 +1,10 @@
 import testFileImage from '../../../../app/images/svg/testcase/file.svg';
 import testFinishImage from '../../../../app/images/svg/testcase/finish.svg';
 import testStartImage from '../../../../app/images/svg/testcase/start.svg';
-
+import testVariantImage from '../../../../app/images/svg/testcase/variant.svg';
 export default abstract class TestImages {
     static readonly Start = testStartImage;
     static readonly File = testFileImage;
     static readonly Finish = testFinishImage;
+    static readonly Variant = testVariantImage;
 }
