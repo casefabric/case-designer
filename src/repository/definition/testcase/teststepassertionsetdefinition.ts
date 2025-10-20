d3cd7a6d012d1fb9d5a934cb300852b4bda6511f
wjgerritsen-0001 <40887209+wjgerritsen-0001@users.noreply.github.com>
2025-10-20 12:39:06 +0200

    Define prececessors on teststepdefinition iso assertionsetdefinition

--- src/repository/definition/testcase/teststepassertionsetdefinition.ts
+++ src/repository/definition/testcase/teststepassertionsetdefinition.ts
@@ -3,37 +3,20 @@ import DocumentableElementDefinition from "../documentableelementdefinition";
 import AssertionDefinition, { CaseInstanceAssertionDefinition } from "./assertiondefinition";
 import TestcaseModelDefinition from "./testcasemodeldefinition";
 import TestStepDefinition from "./teststepdefinition";
-import TestStepPredecessorDefinition from "./teststepprecessordefinition";
-import TestStepVariantDefinition from "./teststepvariantdefinition";
 
 export default abstract class TestStepAssertionSetDefinition extends DocumentableElementDefinition<TestcaseModelDefinition> {
     static XML_ELEMENT = 'assertionset';
 
-    predecessors: TestStepPredecessorDefinition[];
     assertions: AssertionDefinition[];
 
     constructor(importNode: Element, testcase: TestcaseModelDefinition, public parent: TestStepDefinition) {
         super(importNode, testcase, parent);
 
-        this.predecessors = this.parseElements(TestStepPredecessorDefinition.XML_ELEMENT, TestStepPredecessorDefinition);
         this.assertions = parent.constructor.name == 'FinishStepDefinition' ?
             [new CaseInstanceAssertionDefinition(importNode, testcase, this)] : [];
     }
 
-    createPrecessesor(predessor: DocumentableElementDefinition<TestcaseModelDefinition>) {
-        const predecessorDefinition = this.createDefinition(TestStepPredecessorDefinition) as TestStepPredecessorDefinition;
-        predecessorDefinition.sourceRef.update(predessor.id);
-        this.predecessors.push(predecessorDefinition);
-    }
-    removePredecessor(definition: TestStepVariantDefinition) {
-        const predecessorDefinition = this.predecessors.find(p => p.sourceRef.value === definition.id);
-        if (predecessorDefinition) {
-            this.predecessors = this.predecessors.filter(p => p !== predecessorDefinition);
-            predecessorDefinition.removeDefinition();
-        }
-    }
-
     createExportNode(parentNode: Element) {
-        super.createExportNode(parentNode, TestStepAssertionSetDefinition.XML_ELEMENT, 'predecessors');
+        super.createExportNode(parentNode, TestStepAssertionSetDefinition.XML_ELEMENT);
     }
 }    
