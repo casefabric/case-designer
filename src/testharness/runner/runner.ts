cafienne/cafienne-ide

Add fixture definition

src/testharness/runner/runner.ts
@@ -1,73 +1,72 @@
-import { Config as CafienneClientConfig, PlatformService, Tenant, TenantUser, User } from "@cafienne/typescript-client";
+import { Config as CafienneClientConfig, User } from "@cafienne/typescript-client";
 import CaseDefinition from "../../repository/definition/cmmn/casedefinition";
 import TestcaseModelDefinition from "../../repository/definition/testcase/testcasemodeldefinition";
-import Definitions from "../../repository/deploy/definitions";
 import Repository from "../../repository/repository";
+import TestcaseInstance from "./testcaseinstance";
 
 export default class Runner {
     repository: Repository;
 
     constructor(repository: Repository) {
-        console.log("Runner constructor");
         this.repository = repository;
 
         // TODO: Fetch this from Settings in IDE (or as constructor arguments)
         // Set the configuration for the Cafienne client
         CafienneClientConfig.CafienneService.url = 'http://localhost:33027/';
         CafienneClientConfig.TokenService.url = 'http://localhost:33077/token';
-        CafienneClientConfig.TokenService.issuer='http://localhost:33077'
+        CafienneClientConfig.TokenService.issuer = 'http://localhost:33077'
+        CafienneClientConfig.Log.level = 'info';
+        CafienneClientConfig.TestCase.polltimeout = 1000;
+        CafienneClientConfig.CafienneService.log.request.body = false
     }
 
-    async runTests(filePattern: string): Promise<string> {
+    async runTests(filePattern: string): Promise<TestcaseInstance[]> {
         console.group("Test runner");
         console.log(`Running tests for ${filePattern}`);
         const testcases = this.repository.getTestcases()
             .filter(test => test.fileName.match(filePattern))
             .map(testcase => testcase.definition)
-            .filter(testcase => testcase !== undefined) as TestcaseModelDefinition[];
+            .filter(testcase => testcase !== undefined)
+            .flatMap(testcase => [testcase, testcase, testcase, testcase]);
 
-        await this.runTestcases(testcases);
+        const results = await this.runTestcases(testcases);
 
         console.groupEnd();
-        return "success";
+        return results;
     }
 
     async runTestsForCase(caseDefinition: CaseDefinition): Promise<string> {
-        console.group(`Test runner: ${caseDefinition.name}` );
-        const allRelatedDefinitions = new Definitions(caseDefinition);
-        const compiledCase = allRelatedDefinitions.contents();
+        console.group(`Test runner: ${caseDefinition.name}`);
 
         const relevantTestCases = this.repository.getTestcases()
             .map(testcase => testcase.definition)
             .filter(testcase => testcase !== undefined) as TestcaseModelDefinition[];
-        
+
         await this.runTestcases(relevantTestCases);
-            
+
         console.groupEnd();
 
         return "success";
     }
 
-    private async runTestcases(testcases: TestcaseModelDefinition[]): Promise<void> {
-        await Promise.all(testcases.map(x => this.runTestcase(x)));
+    private async runTestcases(testcases: TestcaseModelDefinition[]): Promise<TestcaseInstance[]> {
+        return await Promise.all(testcases.map(x => this.runTestcase(x)));
     }
 
-    private async runTestcase(testcase: TestcaseModelDefinition): Promise<void> {
+    private async runTestcase(testcase: TestcaseModelDefinition): Promise<TestcaseInstance> {
         console.log(`Running testcase: ${testcase.name}`);
 
         const adminUser = await new User("admin").login();
         console.log(`user logged in: ${adminUser}`);
 
-        const tenant = await this.createTenant(adminUser);
+        const instance = await this.setupTestcaseInstance(adminUser, testcase);
+
+        return await instance.run();
     }
 
-    private async createTenant(adminUser: User): Promise<Tenant | undefined> {
-        const tenantOwner = new TenantUser(adminUser.id);
-        tenantOwner.isOwner = true;
-        const tenant = new Tenant("Test-tenant-" + Date.now().toFixed(), [tenantOwner]);
-        await PlatformService.createTenant(adminUser, tenant, 204);
+    private async setupTestcaseInstance(adminUser: User, testcase: TestcaseModelDefinition): Promise<TestcaseInstance> {
+        const instance = new TestcaseInstance(adminUser, testcase);
 
-        return tenant;
+        return instance;
     }
 }
-

src/testharness/runner/testcaseinstance.ts
@@ -1,0 +1,91 @@
+import { assertCasePlan, Case, CaseService, CaseTeam, CaseTeamUser, PlatformService, State, Tenant, TenantUser, User } from "@cafienne/typescript-client";
+
+import CaseDefinition from "../../repository/definition/cmmn/casedefinition";
+import ExternalReference from "../../repository/definition/references/externalreference";
+import TestcaseModelDefinition from "../../repository/definition/testcase/testcasemodeldefinition";
+import Definitions from "../../repository/deploy/definitions";
+
+export default class TestcaseInstance {
+    tenantOwner: TenantUser;
+    tenant: Tenant;
+    steps: Step[];
+    caseInstance?: Case;
+
+    constructor(public adminUser: User, public testcase: TestcaseModelDefinition) {
+        // make admin user tenant owner
+        this.tenantOwner = new TenantUser(this.adminUser.id);
+        this.tenantOwner.isOwner = true;
+        this.tenant = new Tenant("Test-tenant-" + Date.now().toFixed(), [this.tenantOwner]);
+
+        this.steps = [
+            new Step("Fixture setup", () => this.setupFixture()),
+            new Step("Start case", () => this.startCaseInstance()),
+            new Step("Case completed", () => this.caseCompleted())];
+    }
+    async caseCompleted(): Promise<void> {
+        await assertCasePlan(this.tenantOwner, this.caseInstance!, State.Completed);
+    }
+
+    async setupFixture(): Promise<void> {
+
+        // make sure all users are logged in
+        await Promise.all(this.tenant.users.map(async user => {
+            await user.login();
+        }));
+
+
+        await PlatformService.createTenant(this.tenantOwner, this.tenant, 204);
+    }
+
+    async startCaseInstance(): Promise<void> {
+        const caseOwner = new CaseTeamUser(this.tenantOwner);
+        caseOwner.isOwner = true;
+        const caseTeam = new CaseTeam([caseOwner]);
+
+        const compiledCase = compileCase(this.testcase.fixture.caseRef);
+
+        this.caseInstance = await CaseService.startCase(this.tenantOwner, { tenant: this.tenant, definition: compiledCase, inputs: {}, caseTeam, debug: true })
+    }
+
+
+    async run(): Promise<TestcaseInstance> {
+        console.group(`Running testcase: ${this.testcase.name}`);
+
+        for (const step of this.steps) {
+            await step.run();
+
+            if (step.status === "failed") {
+                break;
+            }
+        }
+
+        console.groupEnd();
+
+        return this;
+    }
+}
+
+class Step {
+    status: "pending" | "running" | "passed" | "failed" = "pending";
+    description: string = "";
+
+    constructor(public name: string, public action: () => Promise<void>) {
+    }
+
+    async run(): Promise<void> {
+        console.log(`Running step: ${this.name}`);
+        try {
+            await this.action();
+            this.status = "passed";
+        } catch (error: any) {
+            console.error(`Error in step: ${this.name}`, error);
+            this.status = "failed";
+            this.description = error.toString();
+        }
+    }
+}
+function compileCase(caseRef: ExternalReference<CaseDefinition>): string {
+    const caseDefinitionSet = new Definitions(caseRef.getDefinition()!);
+    const compiledCase = caseDefinitionSet.contents();
+    return compiledCase;
+}

casefabric/case-designer

src/testharness/runner/runner.ts
@@ -1,73 +1,74 @@
-import { Config as CafienneClientConfig, PlatformService, Tenant, TenantUser, User } from "@casefabric/typescript-client";
+import { Config as CafienneClientConfig, User } from "@casefabric/typescript-client";
 import Settings from "../../ide/settings/settings";
 import CaseDefinition from "../../repository/definition/cmmn/casedefinition";
 import TestcaseModelDefinition from "../../repository/definition/testcase/testcasemodeldefinition";
-import Definitions from "../../repository/deploy/definitions";
 import Repository from "../../repository/repository";
+import TestcaseInstance from "./testcaseinstance";
 
 export default class Runner {
     repository: Repository;
 
     constructor(repository: Repository) {
-        console.log("Runner constructor");
         this.repository = repository;
 
         // Set the configuration for the Cafienne client
         CafienneClientConfig.CafienneService.url = Settings.serverURL + '/';
         CafienneClientConfig.TokenService.url = Settings.tokenURL;
         CafienneClientConfig.TokenService.issuer = Settings.issuerURL;
+        CafienneClientConfig.Log.level = 'info';
+        CafienneClientConfig.TestCase.polltimeout = 1000;
+        CafienneClientConfig.CafienneService.log.request.body = false
     }
 
-    async runTests(filePattern: string): Promise<string> {
+    async runTests(filePattern: string): Promise<TestcaseInstance[]> {
         console.group("Test runner");
         console.log(`Running tests for ${filePattern}`);
         const testcases = this.repository.getTestcases()
             .filter(test => test.fileName.match(filePattern))
             .map(testcase => testcase.definition)
-            .filter(testcase => testcase !== undefined) as TestcaseModelDefinition[];
+            .filter(testcase => testcase !== undefined)
+            .flatMap(testcase => [testcase, testcase, testcase, testcase]);
 
-        await this.runTestcases(testcases);
+        const results = await this.runTestcases(testcases);
 
         console.groupEnd();
-        return "success";
+        return results;
     }
 
     async runTestsForCase(caseDefinition: CaseDefinition): Promise<string> {
         console.group(`Test runner: ${caseDefinition.name}`);
-        const allRelatedDefinitions = new Definitions(caseDefinition);
-        const compiledCase = allRelatedDefinitions.contents();
 
         const relevantTestCases = this.repository.getTestcases()
             .map(testcase => testcase.definition)
             .filter(testcase => testcase !== undefined) as TestcaseModelDefinition[];
 
+
         await this.runTestcases(relevantTestCases);
+
 
         console.groupEnd();
 
         return "success";
     }
 
-    private async runTestcases(testcases: TestcaseModelDefinition[]): Promise<void> {
-        await Promise.all(testcases.map(x => this.runTestcase(x)));
+    private async runTestcases(testcases: TestcaseModelDefinition[]): Promise<TestcaseInstance[]> {
+        return await Promise.all(testcases.map(x => this.runTestcase(x)));
     }
 
-    private async runTestcase(testcase: TestcaseModelDefinition): Promise<void> {
+    private async runTestcase(testcase: TestcaseModelDefinition): Promise<TestcaseInstance> {
         console.log(`Running testcase: ${testcase.name}`);
 
         const adminUser = await new User("admin").login();
         console.log(`user logged in: ${adminUser}`);
 
-        const tenant = await this.createTenant(adminUser);
+        const instance = await this.setupTestcaseInstance(adminUser, testcase);
+
+        return await instance.run();
     }
 
-    private async createTenant(adminUser: User): Promise<Tenant | undefined> {
-        const tenantOwner = new TenantUser(adminUser.id);
-        tenantOwner.isOwner = true;
-        const tenant = new Tenant("Test-tenant-" + Date.now().toFixed(), [tenantOwner]);
-        await PlatformService.createTenant(adminUser, tenant, 204);
+    private async setupTestcaseInstance(adminUser: User, testcase: TestcaseModelDefinition): Promise<TestcaseInstance> {
+        const instance = new TestcaseInstance(adminUser, testcase);
 
-        return tenant;
+        return instance;
     }
 }
-

src/testharness/runner/testcaseinstance.ts
@@ -1,0 +1,91 @@
+import { assertCasePlan, Case, CaseService, CaseTeam, CaseTeamUser, PlatformService, State, Tenant, TenantUser, User } from "@casefabric/typescript-client";
+
+import CaseDefinition from "../../repository/definition/cmmn/casedefinition";
+import ExternalReference from "../../repository/definition/references/externalreference";
+import TestcaseModelDefinition from "../../repository/definition/testcase/testcasemodeldefinition";
+import Definitions from "../../repository/deploy/definitions";
+
+export default class TestcaseInstance {
+    tenantOwner: TenantUser;
+    tenant: Tenant;
+    steps: Step[];
+    caseInstance?: Case;
+
+    constructor(public adminUser: User, public testcase: TestcaseModelDefinition) {
+        // make admin user tenant owner
+        this.tenantOwner = new TenantUser(this.adminUser.id);
+        this.tenantOwner.isOwner = true;
+        this.tenant = new Tenant("Test-tenant-" + Date.now().toFixed(), [this.tenantOwner]);
+
+        this.steps = [
+            new Step("Fixture setup", () => this.setupFixture()),
+            new Step("Start case", () => this.startCaseInstance()),
+            new Step("Case completed", () => this.caseCompleted())];
+    }
+    async caseCompleted(): Promise<void> {
+        await assertCasePlan(this.tenantOwner, this.caseInstance!, State.Completed);
+    }
+
+    async setupFixture(): Promise<void> {
+
+        // make sure all users are logged in
+        await Promise.all(this.tenant.users.map(async user => {
+            await user.login();
+        }));
+
+
+        await PlatformService.createTenant(this.tenantOwner, this.tenant, 204);
+    }
+
+    async startCaseInstance(): Promise<void> {
+        const caseOwner = new CaseTeamUser(this.tenantOwner);
+        caseOwner.isOwner = true;
+        const caseTeam = new CaseTeam([caseOwner]);
+
+        const compiledCase = compileCase(this.testcase.fixture.caseRef);
+
+        this.caseInstance = await CaseService.startCase(this.tenantOwner, { tenant: this.tenant, definition: compiledCase, inputs: {}, caseTeam, debug: true })
+    }
+
+
+    async run(): Promise<TestcaseInstance> {
+        console.group(`Running testcase: ${this.testcase.name}`);
+
+        for (const step of this.steps) {
+            await step.run();
+
+            if (step.status === "failed") {
+                break;
+            }
+        }
+
+        console.groupEnd();
+
+        return this;
+    }
+}
+
+class Step {
+    status: "pending" | "running" | "passed" | "failed" = "pending";
+    description: string = "";
+
+    constructor(public name: string, public action: () => Promise<void>) {
+    }
+
+    async run(): Promise<void> {
+        console.log(`Running step: ${this.name}`);
+        try {
+            await this.action();
+            this.status = "passed";
+        } catch (error: any) {
+            console.error(`Error in step: ${this.name}`, error);
+            this.status = "failed";
+            this.description = error.toString();
+        }
+    }
+}
+function compileCase(caseRef: ExternalReference<CaseDefinition>): string {
+    const caseDefinitionSet = new Definitions(caseRef.getDefinition()!);
+    const compiledCase = caseDefinitionSet.contents();
+    return compiledCase;
+}